0a0a350e6e44a5832184aa3d2e80c579410deaf1
masaki rikitoku <masaki.rikitoku@d2c.co.jp>
2015-08-31 10:31:29 +0900

    modify artifactId

--- jawjaw_cli.sh
+++ jawjaw_cli.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cur=$(dirname $0)
-jars=$cur/target/com.masakir-1.0-SNAPSHOT.jar
+jars=$cur/target/jawjaw_cli-1.0-SNAPSHOT.jar
 for jar in $(ls $cur/lib/*.jar) ; do
     jars=$jars:$jar
 done
